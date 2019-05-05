23f514cf0d4b03f478c485272662b0831ab149b8
Farkhad Rizvanov <farkhad.r@rambler.ru>
2019-05-05 23:29:43 +0500

    added warning popup block

--- js/main.js
+++ js/main.js
@@ -216,6 +216,33 @@ $(document).ready(function() {
 
 	//
 
+	// Warning Event
+
+	function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+		var expires = 'expires=' + d.toGMTString();
+		document.cookie = cname + '=' + cvalue + '; ' + expires;
+	}
+	function getCookie(cname) {
+		var name = cname + '=';
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i].trim();
+			if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+		}
+		return '';
+	}
+	if(getCookie('modalWarning') == '') {
+		$(document).on('click', '.js-yes', function() {
+			$('.warning').removeClass('open');
+			setCookie('modalWarning', 'sessionexists', 1);
+		});
+		$('.warning').addClass('open');
+	}
+
+	//
+
 	// Preloader Event
 
 	$(window).on('load', function() {
