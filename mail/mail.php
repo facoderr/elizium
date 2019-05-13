6a6414d10aa114553e65eb49185e38f9a3f576be
Farkhad Rizvanov <farkhad.r@rambler.ru>
2019-05-13 15:04:09 +0500

    added mail.php file

---
+++ mail/mail.php
@@ -0,0 +1,21 @@
+<?php
+
+	$from = 'support@sahar-spa.ru';
+	$to  = 'farkhad.r@rambler.ru';
+	//$to  = 'info@sahar-spa.ru';
+
+	$name = trim($_POST['name']);
+	$phone = trim($_POST['phone']);
+
+	$subject = 'Обратный звонок «Сахар»';
+
+	$message = '<p><strong>Имя:</strong>  ' . $name;
+	$message .= '<p><strong>Телефон:</strong> ' . $phone;
+
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers .= 'From:' . $from . "\r\n";
+
+	mail($to, $subject, $message, $headers);
+
+?>
